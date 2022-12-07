9bf9c9aa9dcb0768766c14faafff9e168c5714f9
antoinepelegrin <antoine.pelegrin@decathlon.com>
2022-12-07 11:51:25 -0500

    k

--- browser_control.py
+++ browser_control.py
@@ -1,21 +1,29 @@
 import webbrowser
+from googlesearch import search
 
-urls = {
+direct_urls = {
     'netflix': 'https://netflix.com',
     'google': 'https://google.com',
     'youtube': 'https://youtube.com',
 }
 
+search_engines = {
 
-def filter_for_key(sentence: str):
+}
+
+def filter_for_key(sentence: str, dictionary: dict):
     words = sentence.split(' ')
     for word in words:
-        if word.lower() in urls:
+        if word.lower() in dictionary:
             return word
     return 'google'
+
+def search_through_google(keyword: str, website: str):
+    return
 
 def open_page(key: str):
     try:
         webbrowser.open(urls[key])
     except KeyError:
         print(f"Can't open {key}")
+
