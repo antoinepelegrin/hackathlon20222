e723b6e9f305089bbeacd2266cee886fa50c5061
antoinepelegrin <antoine.pelegrin@decathlon.com>
2022-12-07 12:26:53 -0500

    Adding google images to url options

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import webbrowser
 urls = {
     'netflix': 'https://netflix.com',
     'google': 'https://google.com',
+    'image': 'https://www.google.com/imghp',
     'youtube': 'https://youtube.com',
     'decathlon': 'https://decathlon.ca'
 }
@@ -11,17 +12,22 @@ urls = {
 def main():
     # Voice to text should go here
     sentence = ''.lower()
+
     words = sentence.split(' ')
     url = filter_for_key(words, urls)
+
     # search
     if 'search' in words:
         christmas = 'christmas' in words
         # user should get prompted for new sentence
         new_sentence = ''
-        search_result = search_through_google(keyword = new_sentence, website = url, christmas = christmas)
+
+        search_result = search_through_google(keyword=new_sentence, website=url, christmas=christmas)
         webbrowser.open(search_result)
+
     # direct connection
     elif {'go', 'to'}.issubset(set(words)):
         webbrowser.open(url)
+
     else:
         print("I don't understand")
