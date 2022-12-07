8ea68e7f866e7e16d658d2d281c9948f5922c086
antoinepelegrin <antoine.pelegrin@decathlon.com>
2022-12-07 14:08:09 -0500

    Cases for youtube and decathlon

--- browser_control.py
+++ browser_control.py
@@ -1,13 +1,39 @@
+import requests
+import bs4
+
+
 def filter_for_key(words: list, dictionary: dict):
     for word in words:
         if word.lower() in dictionary:
-            return dictionary[word.lower()]
-    return dictionary['google']
+            return word.lower()
+    return 'google'
 
 
-def search_through_google(keyword: str, website: str = None, is_christmas: bool = False, is_images: bool = False):
-    christmas = 'christmas ' if is_christmas else ''
-    site = f'site:{website} : ' if website is not None else ''
+def search_through_google(keyword: str, website: str = None, is_images: bool = False):
+    site = f'site:{website} ' if website is not None else ''
     search_option = 'search?tbm=isch&as_q=' if is_images else 'search?q='
-    search_url = f'https://www.google.com/{search_option}{site}{christmas}{keyword}'
+    search_url = f'https://www.google.com/{search_option}{site}{keyword}'
     return search_url
+
+
+def get_decathlon_link(keyword: str):
+    word_list = keyword.split(' ')
+    query = ''
+    for word in word_list:
+        query+= word + '%20'
+    url = f'https://www.decathlon.ca/en/search?query={query}'
+    return url
+
+
+def get_youtube_link(keyword: str):
+    word_list = keyword.split(' ')
+    query = ''
+    for word in word_list:
+        query+= word + '+'
+    url = f'https://www.youtube.com/results?search_query={query}'
+    #request_result = requests.get(search_url)
+    #soup = bs4.BeautifulSoup(request_result.text, "html.parser")
+    #heading_object = soup.find_all('h3')
+
+    #direct_url = ''
+    return url
