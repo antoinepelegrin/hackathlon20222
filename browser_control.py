530b0a4e3fffd7a45a658b29ce1c4e2edbb6857e
antoinepelegrin <antoine.pelegrin@decathlon.com>
2022-12-07 12:24:41 -0500

    basic shape of main file

--- browser_control.py
+++ browser_control.py
@@ -1,29 +1,14 @@
-import webbrowser
 from googlesearch import search
 
-direct_urls = {
-    'netflix': 'https://netflix.com',
-    'google': 'https://google.com',
-    'youtube': 'https://youtube.com',
-}
 
-search_engines = {
-
-}
-
-def filter_for_key(sentence: str, dictionary: dict):
-    words = sentence.split(' ')
+def filter_for_key(words: list, dictionary: dict):
     for word in words:
         if word.lower() in dictionary:
-            return word
-    return 'google'
+            return dictionary[word.lower()]
+    return dictionary['google']
 
-def search_through_google(keyword: str, website: str):
-    return
 
-def open_page(key: str):
-    try:
-        webbrowser.open(urls[key])
-    except KeyError:
-        print(f"Can't open {key}")
-
+def search_through_google(keyword: str, website: str, christmas: bool = False):
+    christmas = 'christmas' if christmas else ''
+    search_results = search(f'site:{website} {christmas} {keyword}')
+    return search_results[0]
